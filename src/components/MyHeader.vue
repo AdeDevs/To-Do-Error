17456da2c2a20fa3662613c1ba77a5d16baa4446
Adeyemi Akinyemi <89089354+AdeDevs@users.noreply.github.com>
2023-01-06 22:58:57 +0100

    Initial commit

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+    <h2>{{ title }}</h2>
+    <Button text="Add Task" color="green" />
+  </header>
+</template>
+
+<script>
+import MyButton from "./MyButton";
+export default {
+  name: "Header",
+  props: ["title"],
+
+  components: {
+    Button: MyButton,
+  },
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
---
+++ src/components/MyHomePage.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <Head title="Task Tracker" />
+    <Tasks :tasks="tasks" />
+  </div>
+</template>
+
+<script>
+import MyHeader from "./MyHeader.vue";
+import MyButton from "./MyButton";
+import Tasks from "./Tasks";
+
+export default {
+  components: {
+    Head: MyHeader,
+    Tasks,
+  },
+  data() {
+    return {
+      tasks: [],
+    };
+  },
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: "Push Code To Github",
+        day: "January 5 at 19:00",
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: "Gaming Session",
+        day: "January 5 at 21:00",
+        reminder: true,
+      },
+      {
+        id: 3,
+        text: "Dinner",
+        day: "January 5 at 22:00",
+        reminder: false,
+      },
+    ];
+  },
+};
+</script>
+
+<style>
+</style>
